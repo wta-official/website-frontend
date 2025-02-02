e19ceb9503e0cf46eedfa3d608a77cb06dbea247
sarmhi <sammiejay813@gmail.com>
2025-02-02 16:12:48 +0100

    deployment: linting errors fix

--- src/components/custome-ui/History.tsx
+++ src/components/custome-ui/History.tsx
@@ -1,46 +1,46 @@
-import Image from 'next/image';
-import { histories } from '@/data/data';
+// import Image from 'next/image';
+// import { histories } from '@/data/data';
 
-const History = () => {
-  return (
-    <div className="flex flex-col lg:flex-row text-white px-4 sm:px-6 md:px-12 lg:px-20 py-10 space-y-8 lg:space-y-0 lg:space-x-20 justify-between">
-      {/* Title */}
-      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl leading-snug sm:leading-normal md:leading-tight lg:leading-tight lg:min-w-[24rem]">
-        Our History
-      </h1>
+// const History = () => {
+//   return (
+//     <div className="flex flex-col lg:flex-row text-white px-4 sm:px-6 md:px-12 lg:px-20 py-10 space-y-8 lg:space-y-0 lg:space-x-20 justify-between">
+//       {/* Title */}
+//       <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl leading-snug sm:leading-normal md:leading-tight lg:leading-tight lg:min-w-[24rem]">
+//         Our History
+//       </h1>
 
-      {/* Scrollable Content */}
-      <div className="no-scrollbar max-w-full md:max-w-[900px] overflow-y-auto h-[400px] sm:h-[500px] md:h-[587px] relative px-4 md:px-8 lg:px-10 py-2">
-        <div>
-          {histories.map((history, index) => (
-            <div
-              key={index}
-              className="relative px-4 sm:px-6 md:px-8 md:pb-8 lg:px-10 border-l border-[#474747]"
-            >
-              <div className="space-y-4 sm:space-y-6 md:space-y-8 lg:space-y-10 p-4 sm:p-6 md:p-8 lg:p-10 history-bg">
-                <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl">
-                  {history.date}
-                </h2>
-                <p className="text-sm sm:text-base md:text-lg lg:text-2xl text-gray-300">
-                  {history.history}
-                </p>
-              </div>
+//       {/* Scrollable Content */}
+//       <div className="no-scrollbar max-w-full md:max-w-[900px] overflow-y-auto h-[400px] sm:h-[500px] md:h-[587px] relative px-4 md:px-8 lg:px-10 py-2">
+//         <div>
+//           {histories.map((history, index) => (
+//             <div
+//               key={index}
+//               className="relative px-4 sm:px-6 md:px-8 md:pb-8 lg:px-10 border-l border-[#474747]"
+//             >
+//               <div className="space-y-4 sm:space-y-6 md:space-y-8 lg:space-y-10 p-4 sm:p-6 md:p-8 lg:p-10 history-bg">
+//                 <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl">
+//                   {history.date}
+//                 </h2>
+//                 <p className="text-sm sm:text-base md:text-lg lg:text-2xl text-gray-300">
+//                   {history.history}
+//                 </p>
+//               </div>
 
-              <div className="absolute -top-2  left-0 -ml-3 sm:-ml-4 md:-ml-5 lg:-ml-6 z-50">
-                <Image
-                  src="/images/checkmark-badge-02.svg"
-                  alt="checkbox"
-                  width={40}
-                  height={40}
-                  className="w-5 h-5 sm:w-6 sm:h-6 md:w-8 md:h-8 lg:w-10 lg:h-10 z-50"
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
+//               <div className="absolute -top-2  left-0 -ml-3 sm:-ml-4 md:-ml-5 lg:-ml-6 z-50">
+//                 <Image
+//                   src="/images/checkmark-badge-02.svg"
+//                   alt="checkbox"
+//                   width={40}
+//                   height={40}
+//                   className="w-5 h-5 sm:w-6 sm:h-6 md:w-8 md:h-8 lg:w-10 lg:h-10 z-50"
+//                 />
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default History;
+// export default History;
--- src/components/custome-ui/SearchTalents.tsx
+++ src/components/custome-ui/SearchTalents.tsx
@@ -1,19 +1,22 @@
-"use client";
+'use client';
 
-import { Search } from "lucide-react";
-import { Input } from "../ui/input";
-import { useState } from "react";
-import { ourTalents } from "@/data/data";
-import TalentsCard from "./TalentsCard";
+import { Search } from 'lucide-react';
+import { Input } from '../ui/input';
+import { useState } from 'react';
+import TalentsCard from './TalentsCard';
 
-const AlphabetSearch = ({ onFilter }: { onFilter: (letter: string) => void }) => {
+const AlphabetSearch = ({
+  onFilter,
+}: {
+  onFilter: (letter: string) => void;
+}) => {
   return (
     <div className="w-full mx-auto mt-5">
       {/* Alphabet Buttons */}
       <div className="flex items-center justify-center space-x-2 flex-wrap">
         <p className="self-start my-2 text-center w-full md:w-auto">#</p>
         <div className="flex flex-wrap justify-center space-x-2">
-          {[..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"].map((letter) => (
+          {[...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'].map((letter) => (
             <button
               key={letter}
               className="px-[5px] py-2 rounded-md text-gray-500 hover:text-white transition duration-200"
@@ -29,8 +32,8 @@ const AlphabetSearch = ({ onFilter }: { onFilter: (letter: string) => void }) =>
   );
 };
 
-const SearchTalents = ({talents}) => {
-  const [searchQuery, setSearchQuery] = useState("");
+const SearchTalents = ({ talents }) => {
+  const [searchQuery, setSearchQuery] = useState('');
   const filteredData = talents.filter((item) =>
     item.name.toLowerCase().startsWith(searchQuery.toLowerCase())
   );
