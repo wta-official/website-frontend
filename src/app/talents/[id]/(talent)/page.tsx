9df30ab8feefc78967ccbb105a9e04be06742b1a
sarmhi <sammiejay813@gmail.com>
2025-02-03 16:58:46 +0100

    deployment: dynamic metadata issue

--- src/app/talents/[id]/(talent)/page.tsx
+++ src/app/talents/[id]/(talent)/page.tsx
@@ -2,21 +2,19 @@ import TalentPage from '@/components/Talent';
 import { Metadata } from 'next';
 
 type Props = {
-  params: Promise<{ id?: string }>;
+  params: { id?: string };
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const resolvedParams = await params;
   return {
-    title: `Talent: ${resolvedParams.id}`,
+    title: `Talent: ${params.id}`,
   };
 }
 
 const Talent = async ({ params }: Props) => {
-  const resolvedParams = await params;
   return (
     <div>
-      <TalentPage params={resolvedParams} />
+      <TalentPage params={params} />
     </div>
   );
 };
